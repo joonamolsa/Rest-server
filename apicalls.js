a8776138e8dd263231fdd26b2892ccca67eafb6a
Joona Mölsä <2306075@edu.karelia.fi>
2025-04-12 23:31:21 +0300

    console.log apicalls

--- apicalls.js
+++ apicalls.js
@@ -1,4 +1,5 @@
-console.log("apicalls.js loaded");
+console.log("✅ apicalls.js loaded");
+
 import express from "express";
 import { verifyToken } from "./utils.js";
 import cors from "cors";
@@ -18,5 +19,32 @@ app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", index);
 app.use("/data", verifyToken, data);
 app.use("/login", login);
+
+// 🔍 TULOSTA KAIKKI KÄYTÖSSÄ OLEVAT REITIT
+function listRoutes(app) {
+  const routes = [];
+  app._router.stack.forEach((middleware) => {
+    if (middleware.route) {
+      routes.push(middleware.route);
+    } else if (middleware.name === "router") {
+      middleware.handle.stack.forEach((handler) => {
+        const route = handler.route;
+        if (route) {
+          routes.push(route);
+        }
+      });
+    }
+  });
+
+  console.log("🔎 Rekisteröidyt reitit:");
+  routes.forEach((r) => {
+    const methods = Object.keys(r.methods)
+      .map((m) => m.toUpperCase())
+      .join(", ");
+    console.log(`  ${methods} ${r.path}`);
+  });
+}
+
+listRoutes(app);
 
 export default app;
