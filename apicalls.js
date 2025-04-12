2fd422c061a5b9a2c32c41e53773f7d3137b40ec
Joona Mölsä <2306075@edu.karelia.fi>
2025-04-12 23:17:11 +0300

    console.log apicalls

--- apicalls.js
+++ apicalls.js
@@ -1,21 +1,22 @@
-import express from 'express';
-import {verifyToken} from './utils.js'
+console.log("apicalls.js loaded");
+import express from "express";
+import { verifyToken } from "./utils.js";
 import cors from "cors";
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import index from './api/index.js';
-import data from './api/data.js';
-import login from './api/login.js';
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import index from "./api/index.js";
+import data from "./api/data.js";
+import login from "./api/login.js";
 
-const swaggerDocument = YAML.load('./openapi/api.yaml');
+const swaggerDocument = YAML.load("./openapi/api.yaml");
 const app = express();
 app.use(express.json());
 
 app.use(cors());
 
-app.use('/doc', swaggerUi.serve,   swaggerUi.setup(swaggerDocument));
-app.use('/', index);
-app.use('/data', verifyToken, data);
-app.use('/login', login);
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/", index);
+app.use("/data", verifyToken, data);
+app.use("/login", login);
 
 export default app;
