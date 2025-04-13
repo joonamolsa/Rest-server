9589224950919466cbc4357b25c9b49858457423
Joona Mölsä <2306075@edu.karelia.fi>
2025-04-13 15:11:47 +0300

    login parannus

--- api/login.js
+++ api/login.js
@@ -4,23 +4,26 @@ import { logonUsers, findOneUser } from "../database.js";
 let router = Router();
 
 router.post("/", async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  let user = await findOneUser(username);
-  console.log(user[0].username);
-  if (user[0] && user[0].password === password) {
-    const token = jwt.sign({ username: user[0].username }, "my_secret_key", {
-      expiresIn: "1h",
-    });
-    logonUsers.set(username, { ...user[0], token: token });
-    console.log(logonUsers.get(username));
-    res.json({
-      username: username,
-      access_token: token,
-      token_type: "Bearer",
-      expires_in: "1h",
-    });
-  } else res.status(401).json({ error: "Login failed" });
-});
+  const { username, password } = req.body;
+  try {
+    const user = await findOneUser(username);
+    if (!user[0]) return res.status(401).json({ error: "User not found" });
 
-export default router;
+    if (user[0].password === password) {
+      const token = jwt.sign({ username }, "my_secret_key", {
+        expiresIn: "1h",
+      });
+      logonUsers.set(username, { ...user[0], token });
+      res.json({
+        username,
+        access_token: token,
+        token_type: "Bearer",
+        expires_in: "1h",
+      });
+    } else {
+      res.status(401).json({ error: "Invalid password" });
+    }
+  } catch (e) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
